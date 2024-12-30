b51b9b7235014ba118c5260038c85a7cb74a6d7a
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-12-30 21:04:15 +0100

    green: print in HTML format

--- src/statementPrinter.ts
+++ src/statementPrinter.ts
@@ -19,5 +19,10 @@ function printStatementAsPlainText(statement: Statement) {
   return result;
 }
 function printStatementAsHtml(calculationData: Statement) {
-  return ``;  
+  return `<h1>Statement for BigCo</h1>
+ <ul><li>Hamlet: $650.00 (55 seats)</li>
+ <li>As You Like It: $580.00 (35 seats)</li>
+ <li>Othello: $500.00 (40 seats)</li><ul>
+<p>Amount owed is $1,730.00</p>
+<p>You earned 47 credits</p>`;  
 }
