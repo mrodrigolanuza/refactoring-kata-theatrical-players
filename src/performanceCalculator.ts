e8ac645c40b3d9f6b567438930d06137d6e7af88
Marcos Rodrigo <marcosrodrigolanuza@gmail.com>
2024-12-30 20:52:05 +0100

    refactor: remove switch clause

--- src/performanceCalculator.ts
+++ src/performanceCalculator.ts
@@ -1,4 +1,4 @@
-enum PlayType {
+export enum PlayType {
     Tragedy = "tragedy",
     Comedy  = "comedy"
 }
@@ -21,39 +21,43 @@ export function calculateTotalAmount(summary: PerformanceSummary, plays: Record<
         .map(perf => calculateAmount(plays[perf.playID], perf))
         .reduce((acc, cur) => acc + cur, 0);
 }
-
 export function calculateAmount(play: Play, performance: Performance) {
-    let totalAmount = 0;
-    switch (play.type) {
-        case PlayType.Tragedy:
-            totalAmount = 40000;
-            if (performance.audience > 30) {
-                totalAmount += 1000 * (performance.audience - 30);
-            }
-            break;
-        case PlayType.Comedy:
-            totalAmount = 30000;
-            if (performance.audience > 20) {
-                totalAmount += 10000 + 500 * (performance.audience - 20);
-            }
-            totalAmount += 300 * performance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${play.type}`);
+    const IsNotValidPlayType = play.type != PlayType.Tragedy && play.type != PlayType.Comedy;
+    if(IsNotValidPlayType) 
+         throw new Error(`unknown type: ${play.type}`);
+    
+    if (play.type === PlayType.Tragedy) {
+        return calculateAmountForTragedy(performance);
+    }
+    if (play.type === PlayType.Comedy) {
+        return calculateAmountForComedy(performance);
+    }
+    return 0;
+}
+const calculateAmountForTragedy = (performance: Performance) => {
+    let totalAmount = 40000;
+    if (performance.audience > 30) {
+        totalAmount += 1000 * (performance.audience - 30);
     }
     return totalAmount;
 }
-
-export function calculateCreditsFor(play: Play, perf: Performance) {
+const calculateAmountForComedy = (performance: Performance) => {
+    let totalAmount = 30000;
+    if (performance.audience > 20) {
+        totalAmount += 10000 + 500 * (performance.audience - 20);
+    }
+    totalAmount += 300 * performance.audience;
+    return totalAmount;
+}
+export function calculateVolumeCredits(summary: PerformanceSummary, plays: Record<string, Play>) {
+    return summary.performances
+        .map(perf => calculateCreditsFor(plays[perf.playID], perf))
+        .reduce((acc, cur) => acc + cur, 0);
+}
+function calculateCreditsFor(play: Play, perf: Performance) {
     let baseCredits = Math.max(perf.audience - 30, 0);
     let extraComedyCredits = Math.floor(perf.audience / 5);
     return ("comedy" === play.type)
         ? baseCredits + extraComedyCredits
         : baseCredits;
-}
-
-export function calculateVolumeCredits(summary: PerformanceSummary, plays: Record<string, Play>) {
-    return summary.performances
-        .map(perf => calculateCreditsFor(plays[perf.playID], perf))
-        .reduce((acc, cur) => acc + cur, 0);
 }
